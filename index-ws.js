dd121c1a561717dfe06517f7e62765e28492f438
Bota Andrei Cristian <andreibota188@gmail.com>
2026-09-10 18:09:39 +0300

    update

--- index-ws.js
+++ index-ws.js
@@ -37,12 +37,8 @@ wss.on('connection', function connection(ws) {
 
     if (ws.readyState === ws.OPEN) {
         ws.send('Welcome to my server');
-          const oraLocalaAcum = new Date().toLocaleTimeString('ro-RO', { 
-            hour: '2-digit', 
-            minute: '2-digit' 
-        });
-        ws.send(`Ora exactă este: ${oraLocalaAcum}`);
-    }
+         ws.send(`TIMESTAMP:${Date.now()}`);
+        }
 
     db.run(`INSERT INTO visitors (count, time)
         VALUES (${numClients}, datetime('now'))
